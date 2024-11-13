308676bffa1ba718f33726caf5097471041be6ae
Angelo Luz <angelogl@gmail.com>
2024-11-13 15:28:48 -0300

    Altera api model para gpt 4

--- src/exercicio0/Fornecedor.ts
+++ src/exercicio0/Fornecedor.ts
@@ -1,7 +1,7 @@
 import { Usuario } from "./Usuario";
 
 export class Fornecedor extends Usuario{
-
+    
     constructor(login: string, senha: string, cnpj: string){
         super(login, senha, cnpj);
     }
